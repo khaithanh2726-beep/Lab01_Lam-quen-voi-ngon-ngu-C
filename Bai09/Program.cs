b90b3883243e7013dca6cc34fe838f9a69f002e4
KhaiThanh <khaithanh2726@gmail.com>
2026-09-17 00:08:20 +0700

    Xay dung phuong thuc tim gia tri lon nhat va nho nhat bang out

---
+++ Bai09/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace bai09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("So thuc thu nhat: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("So thuc thu hai: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("So thuc thu ba: ");
+            double c = double.Parse(Console.ReadLine());
+            double max, min; 
+            FindMaxMin(a, b, c, out max, out min); 
+            Console.WriteLine($"Max: {max}");
+            Console.WriteLine($"Min: {min}");
+        }
+        static void FindMaxMin(double x, double y, double z, out double max, out double min)
+        //FindMaxMin tra ve max, min
+        {
+            max = Math.Max(x, Math.Max(y, z));
+            min = Math.Min(x, Math.Min(y, z));
+        }
+    }
+}
