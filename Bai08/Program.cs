a9a51d90cfac9e7385e817ad3dc4947349c6d8a9
KhaiThanh <khaithanh2726@gmail.com>
2026-09-17 00:07:37 +0700

    Xay dung phuong thuc hoan vi hai so thuc bang ref

---
+++ Bai08/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Bai08
+{
+    class XuLy
+    {
+        //Hoan vi 
+        public void HoanVi(ref double a, ref double b)
+        {
+            double temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XuLy xl = new XuLy();
+            double x = 1.5, y = 4.8;
+            xl.HoanVi(ref x, ref y);
+            Console.WriteLine($"Sau khi hoan vi: x = {x}, y = {y}");
+            Console.ReadKey();
+        }
+    }
+}
