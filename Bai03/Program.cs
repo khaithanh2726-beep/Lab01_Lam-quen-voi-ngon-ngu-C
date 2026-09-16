d817f6f7a361509328096923e1ba9248b1747b17
KhaiThanh <khaithanh2726@gmail.com>
2026-09-16 20:18:51 +0700

    Tuan01/Bai03/Nhap hai so nguyen x y va tinh x mu y

---
+++ Bai03/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Bai03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Nhhap x
+            Console.Write("Nhap so nguyen x: ");
+            int x = int.Parse(Console.ReadLine());
+            // Nhap y
+            Console.Write("Nhap so nguyen y: ");
+            int y = int.Parse(Console.ReadLine());
+            double ketQua = Math.Pow(x, y);
+            Console.WriteLine($"Ket qua {x} mu {y} la: {ketQua}");
+            Console.ReadKey();
+        }
+    }
+}
