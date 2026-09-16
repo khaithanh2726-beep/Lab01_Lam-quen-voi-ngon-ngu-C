9333233b8f55e324de2ab29797af99d97da2dc71
KhaiThanh <khaithanh2726@gmail.com>
2026-09-17 00:03:49 +0700

    Nhap hai so nguyen va kiem tra du lieu hop le

---
+++ Bai04/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Bai04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            // Kiem tra so nguyen x va y nhap vao
+            while (true)
+            {
+                Console.Write("Nhap so nguyen x: ");
+                if (int.TryParse(Console.ReadLine(), out x))
+                {
+                    break; 
+                }
+                Console.WriteLine("Loi: Gia tri nhap vao khong phải la so nguyen! Vui long nhap lai.");
+            }
+            while (true)
+            {
+                Console.Write("Nhap so nguyen y: ");
+                if (int.TryParse(Console.ReadLine(), out y))
+                {
+                    break;
+                }
+                Console.WriteLine("Loi: Gia tri nhap vao khong phải la so nguyen! Vui long nhap lai.");
+            }
+            double ketQua = Math.Pow(x, y);
+            Console.WriteLine($"Ket qua {x} mu {y} la: {ketQua}");
+            Console.ReadKey();
+        }
+    }
+}
