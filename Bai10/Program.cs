8ea6aa800ed333c00b3829db813fed7a5eb7ae49
KhaiThanh <khaithanh2726@gmail.com>
2026-09-17 00:09:04 +0700

    Xay dung phuong thuc kiem tra chuoi doi xung

---
+++ Bai10/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace bai10
+{
+    class Program
+    {
+        public static bool ChuoiDoiXUng(string str)
+        {
+            int left = 0;
+            int right = str.Length - 1; 
+            while (left < right) //Vong lap cho trai = phai
+            {
+                if (str[left] != str[right])
+                {
+                    return false; 
+                }
+                left++; 
+                right--;
+            }
+            return true; 
+        }
+        public static void Main(string[] args)
+        {
+            Console.Write("Nhập chuỗi: ");
+            string str = Console.ReadLine(); 
+            if (ChuoiDoiXUng(str)) //Ktr chuoi doi xung
+            {
+                Console.WriteLine("Chuỗi đối xứng"); 
+            }
+            else
+            {
+                Console.WriteLine("Chuỗi không đối xứng"); 
+            }
+        }
+    }
+}
