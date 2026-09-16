1e425917c77eb73c7a3625ddd5e24f201a0578c1
KhaiThanh <khaithanh2726@gmail.com>
2026-09-17 00:06:35 +0700

    Xay dung phuong thuc kiem tra so nguyen to bang bool

---
+++ Bai07/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Bai07
+{
+    class KiemTra
+    {
+        //Ktr so nguyen to
+        public bool LaSoNguyenTo(int n)
+        {
+            if (n < 2) return false;
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+                if (n % i == 0) return false;
+            return true;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KiemTra kt = new KiemTra();
+            int n = 7;
+            Console.WriteLine($"{n} co phai la SNT? {kt.LaSoNguyenTo(n)}");
+            Console.ReadKey();
+        }
+    }
+}
