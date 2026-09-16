01797decace0cf58f1ea63a02c048d2de240b0c4
KhaiThanh <khaithanh2726@gmail.com>
2026-09-16 20:16:28 +0700

    Tuan 01/Bai 01/Nhap ho ten va xuat ho ten ra man hinh console xem msil file PE trong B1 và luu MSIL nay sang file *.IL

---
+++ Bai01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Bai01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap ho ten cua ban: ");
+            string hoTen = Console.ReadLine();
+            Console.WriteLine($"Chao ban {hoTen}!");
+            Console.ReadKey();
+        }
+    }
+}
