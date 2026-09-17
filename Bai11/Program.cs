9048ae13b83aa58d8c532aff33047ed1a86b1fc8
KhaiThanh <khaithanh2726@gmail.com>
2026-09-17 09:12:11 +0700

    Xay dung phuong thuc dao nguoc mot chuoi

---
+++ Bai11/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Bai11
+{
+    public class Program
+    {
+        public static string ChuoiDaoNguoc(string str)
+        {
+            char[] charArray = str.ToCharArray(); //Chuyen thanh chuoi ky tu
+            Array.Reverse(charArray); //Dao nguoc mang ky tu
+            return new string(charArray); 
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.Write("Nhập chuỗi: ");
+            string str = Console.ReadLine();
+            str = ChuoiDaoNguoc(str); 
+            Console.WriteLine("Chuỗi đảo ngược: " + str); 
+        }
+    }
+}
