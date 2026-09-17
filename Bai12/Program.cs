36c48c2bfd9ade3a8491e74d3a4d3ac8145b4e2f
KhaiThanh <khaithanh2726@gmail.com>
2026-09-17 09:12:49 +0700

    Chuyen chuoi sang chu thuong, chu hoa va dem so tu

---
+++ Bai12/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Bai12
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Nhap chuoi: ");
+            string text = Console.ReadLine();
+            string chuoiThuong = text.ToLower();// Chuyen sang chua hoa va thuong
+            string chuoiHoa = text.ToUpper();
+            string[] danhSachTu = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries); //Dem so tu trong chuoi
+            int soTu = danhSachTu.Length;
+            Console.WriteLine($"Chuoi chu thuong: {chuoiThuong}");
+            Console.WriteLine($"Chuoi chu hoa: {chuoiHoa}");
+            Console.WriteLine($"So tu trong chuoi: {soTu}");
+        }
+    }
+}
